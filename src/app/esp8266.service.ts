ea8bd2d049430c0dc0f135e002b91af13d5d3112
asaucedo777@gmail.com <asaucedo777@gmail.com>
2020-05-30 19:27:54 +0200

    Actualizar API

--- src/app/esp8266.service.ts
+++ src/app/esp8266.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Pin } from './pin.model';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class Esp8266Service {
@@ -9,42 +10,51 @@ export class Esp8266Service {
   constructor(public httpClient: HttpClient) { }
 
   public test(): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/test');
+    return this.httpClient.get(environment.API_URL + '/TEST');
+  }
+  public getTime(): Observable<any> {
+    return this.httpClient.get(environment.API_URL + '/TIME');
+  }
+
+  public ledBuiltinStatus(): Observable<any> {
+    return this.httpClient.get(environment.API_URL + '/LED_BUILTIN');
   }
   public ledBuiltinSwitch(): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/LED_BUILTIN/SWITCH');
+    return this.httpClient.get(environment.API_URL + '/LED_BUILTIN/SWITCH');
   }
   public ledBuiltinOn(): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/LED_BUILTIN/ON');
+    return this.httpClient.get(environment.API_URL + '/LED_BUILTIN/ON');
   }
   public ledBuiltinOff(): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/LED_BUILTIN/OFF');
+    return this.httpClient.get(environment.API_URL + '/LED_BUILTIN/OFF');
   }
-  public time(): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/TIME');
+
+  public digitalPins(): Observable<any> {
+    return this.httpClient.get(environment.API_URL + '/DIGITAL_PINS');
   }
-  public digitalPinSwitch(pin): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/DIGITAL_PIN/SWITCH?pin=' + pin);
+  public digitalPinGet(pin: number): Observable<Pin> {
+    return this.httpClient.get<Pin>(environment.API_URL + '/DIGITAL_PIN_GET?pin=' + pin);
   }
-  public digitalPinOn(pin): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/DIGITAL_PIN/ON?pin=' + pin);
+  public digitalPinPost(pinData: Pin): Observable<Pin> {
+    return this.httpClient.post<Pin>(environment.API_URL + '/DIGITAL_PIN_POST', pinData);
   }
-  public digitalPinOff(pin): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/DIGITAL_PIN/OFF?pin=' + pin);
+  public digitalPinStatus(pin: number): Observable<any> {
+    return this.httpClient.get(environment.API_URL + '/DIGITAL_PIN/STATUS?pin=' + pin);
   }
-  public digitalPinSchedule(): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/DIGITAL_PIN/SCHEDULE');
+  public digitalPinSwitch(pin: number): Observable<any> {
+    return this.httpClient.get(environment.API_URL + '/DIGITAL_PIN/SWITCH?pin=' + pin);
   }
-  public digitalPinSchedules(): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/DIGITAL_PIN/SCHEDULES');
+  public digitalPinOn(pin: number): Observable<any> {
+    return this.httpClient.get(environment.API_URL + '/DIGITAL_PIN/ON?pin=' + pin);
   }
-  public digitalPinScheduleSwitch(pinData: Pin): Observable<any> {
-    return this.httpClient.post('http://192.168.0.23/DIGITAL_PIN/SCHEDULE', pinData);
+  public digitalPinOff(pin: number): Observable<any> {
+    return this.httpClient.get(environment.API_URL + '/DIGITAL_PIN/OFF?pin=' + pin);
   }
-  public digitalPinScheduled(): Observable<any> {
-    return this.httpClient.get('http://192.168.0.23/DIGITAL_PIN/SCHEDULED');
+
+  public scheduledGet(): Observable<any> {
+    return this.httpClient.get(environment.API_URL + '/SCHEDULED');
   }
-  public digitalPinScheduledSwitch(scheduled: any): Observable<any> {
-    return this.httpClient.post('http://192.168.0.23/DIGITAL_PIN/SCHEDULED', scheduled);
+  public scheduledSwitch(): Observable<any> {
+    return this.httpClient.get(environment.API_URL + '/SCHEDULED/SWITCH');
   }
 }
